4ba8222722025eb5b3a97cfe0c9354a2cc50a272
JuliaKuznetsovaQA <kuznetsovajulia@gmail.com>
2024-06-15 18:20:59 +0300

    Changes to Lesson4.java

--- src/Lesson4.java
+++ src/Lesson4.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class Lesson4 {
     public static void main(String[] args) {
 
@@ -10,5 +12,23 @@ public class Lesson4 {
             System.out.println(a);
             a = a + 1;
         } while (a < 5);
+
+        int[] arr = {1, 2, 3, 4, 5};
+        System.out.println(Arrays.toString(arr));
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+
+        int [] array = new int[1000];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = i;
+        }
+        System.out.println(Arrays.toString(array));
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] % 2 == 0) {
+                array[i] = array[i] * 10;
+            }
+        }
+        System.out.println(Arrays.toString(array));
     }
 }
