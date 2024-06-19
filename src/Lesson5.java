391fc3b82cf40b20b2cfea2f78dfffa3a2dc35cf
JuliaKuznetsovaQA <kuznetsovajulia@gmail.com>
2024-06-20 00:44:09 +0300

    changes to Lesson5.java

--- src/Lesson5.java
+++ src/Lesson5.java
@@ -1,8 +1,8 @@
 public class Lesson5 {
     public static void main(String[] args) {
 
-        int[] arr = {1, 5, 8, 3, 6, 9};
-        int max = arr[0];
+        int[] arr = {2, 5, 8, 3, 1, 9};
+        int max = Integer.MIN_VALUE;
 
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] > max) {
@@ -10,5 +10,32 @@ public class Lesson5 {
             }
         }
         System.out.println(max);
+
+        System.out.println("----- SUM OF ARRAY ------");
+
+        int sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            sum += arr[i];
+        }
+        System.out.println(sum);
+
+        System.out.println("----- MAX OF 2D ARRAY ------");
+
+        int [][] arr2d = {
+            {1, 3, 6},
+            {7, 9, 17},
+            {3, 8, 23}
+        };
+
+        int max1 = Integer.MIN_VALUE;
+        for (int i = 0; i < arr2d.length; i++) {
+            for (int j = 0; j < arr2d[i].length; j++) {
+                if (arr2d[i][j] > max1) {
+                    max1 = arr2d[i][j];
+                }
+            }
+        }
+        System.out.println(max1);
+
     }
 }
