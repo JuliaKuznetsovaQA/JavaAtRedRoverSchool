0a48c796d99b2babe3707aff2a3b346f34c807d6
JuliaKuznetsovaQA <kuznetsovajulia@gmail.com>
2024-07-07 10:30:18 +0300

    Lesson13 added

---
+++ src/Lesson3/Main.java
@@ -0,0 +1,19 @@
+package Lesson3;
+
+public class Main {
+
+    public static int getSum(int a, int b) {
+        return a + b;
+    }
+
+    public static void testGetSum() {
+        System.out.println(getSum(0, 0) == 0);
+        System.out.println(getSum(5, 5) == 10);
+        System.out.println(getSum(1000000000, 1000000000) == 2000000000);
+        System.out.println(getSum(-5, -5) == -10);
+    }
+
+    public static void main(String[] args) {
+        testGetSum();
+    }
+}
